18c6c9cd04ef0b268e4022fbf7965476adaa7ee2
Laxman Rumba <rumbavai767@gmail.com>
2025-12-15 14:47:05 +0545

    chore: set up folder structure

---
+++ src/app.module.ts
@@ -0,0 +1,65 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoreModule } from './core/core.module';
+import dataSource from './data-source';
+import { plainToInstance } from 'class-transformer';
+import { EnvConfig } from './config/env.validation';
+import { validateSync } from 'class-validator';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { BullModule } from '@nestjs/bullmq';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(dataSource.options),
+    CoreModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV || 'production'}`],
+      validate: (env) => {
+        const envConfig = plainToInstance(EnvConfig, env, {
+          enableImplicitConversion: true,
+        });
+        const errors = validateSync(envConfig, {
+          skipMissingProperties: false,
+        });
+
+        if (errors.length > 0) {
+          console.error(
+            'Invalid environment variables:',
+            errors.map((err) =>
+              err.constraints
+                ? Object.values(err.constraints).join(', ')
+                : 'Unknown validation error',
+            ),
+          );
+          throw new Error('Invalid environment variables');
+        }
+        return envConfig;
+      },
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT') || 6379,
+        },
+        defaultJobOptions: { attempts: 3 },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/config/env.validation.ts
@@ -0,0 +1,56 @@
+import { IsString, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class EnvConfig {
+  @IsString()
+  DB_HOST: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  DB_PORT: number;
+
+  @IsString()
+  DB_USER: string;
+
+  @IsString()
+  DB_PASSWORD: string;
+
+  @IsString()
+  DB_NAME: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  PORT: number;
+
+  @IsString()
+  REDIS_HOST: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  REDIS_PORT: number;
+
+  @IsString()
+  JWT_SECRET: string;
+
+  @IsString()
+  JWT_EXPIRATION_TIME: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  EMAIL_PORT: number;
+
+  @IsString()
+  EMAIL_HOST: string;
+
+  @IsString()
+  EMAIL_USER: string;
+
+  @IsString()
+  EMAIL_PASS: string;
+
+  @IsString()
+  EMAIL_CONFIRMATION_URL: string;
+
+  @IsString()
+  EMAIL_DOMAIN_ADDRESS: string;
+}
