81269a33303430c339f805cec6b4d92d5f11681e
Laxman Rumba <rumbavai767@gmail.com>
2025-12-18 10:04:49 +0545

    refact: updated the user entity and migration

--- src/migrations/1765967976599-createUserTable.ts
+++ src/migrations/1765967976599-createUserTable.ts
@@ -5,20 +5,18 @@ export class CreateUserTable1765967976599 implements MigrationInterface {
     await queryRunner.query(`
       CREATE TABLE "users" (
         "user_id" varchar PRIMARY KEY,
-        "username" varchar UNIQUE,
-        "first_name" varchar,  
-        "last_name" varchar, 
+        "email" varchar(255) UNIQUE NOT NULL,
+        "first_name" varchar(255),  
+        "last_name" varchar(255), 
         "primary_location" varchar(255),
         "contact_number" varchar(255)
       );
-      CREATE INDEX "IDX_users_username" ON "users" ("username");
       CREATE INDEX "IDX_users_primary_location" ON "users" ("primary_location");
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      DROP INDEX IF EXISTS "IDX_users_username";
       DROP INDEX IF EXISTS "IDX_users_primary_location";
       DROP TABLE "users";`);
   }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,6 +1,5 @@
 import { Entity, Column, Index, PrimaryColumn } from 'typeorm';
 
-@Index('IDX_users_username', ['username'])
 @Index('IDX_users_primary_location', ['primaryLocation'])
 @Entity({ name: 'users' })
 export class User {
@@ -8,17 +7,27 @@ export class User {
   readonly userId: string;
 
   @Column({ type: 'varchar', length: 255, unique: true })
-  readonly username: string;
+  readonly email: string;
 
-  @Column({ type: 'varchar', length: 255, name: 'first_name' })
-  readonly firstName: string;
+  @Column({ type: 'varchar', length: 255, name: 'first_name', nullable: true })
+  readonly firstName: string | null;
 
-  @Column({ type: 'varchar', length: 255, name: 'last_name' })
-  readonly lastName: string;
+  @Column({ type: 'varchar', length: 255, name: 'last_name', nullable: true })
+  readonly lastName: string | null;
 
-  @Column({ type: 'varchar', length: 255, name: 'primary_location' })
-  readonly primaryLocation: string;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    name: 'primary_location',
+    nullable: true,
+  })
+  readonly primaryLocation: string | null;
 
-  @Column({ type: 'varchar', length: 255, name: 'contact_number' })
-  readonly contactNumber: string;
+  @Column({
+    type: 'varchar',
+    length: 255,
+    name: 'contact_number',
+    nullable: true,
+  })
+  readonly contactNumber: string | null;
 }
