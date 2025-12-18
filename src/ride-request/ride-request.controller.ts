84a84ce32ea98f9e758213971267de686daaa1ee
Laxman Rumba <rumbavai767@gmail.com>
2025-12-18 15:31:37 +0545

    chore: add ride request update service and controller

--- src/ride-request/ride-request.controller.ts
+++ src/ride-request/ride-request.controller.ts
@@ -3,6 +3,9 @@ import {
   Controller,
   HttpCode,
   HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Patch,
   Post,
   Req,
   UseGuards,
@@ -11,6 +14,7 @@ import { RideRequestService } from './ride-request.service';
 import { CreateRideRequestData } from './dto/create-ride-request-data';
 import { AuthGuardService } from '@/auth-guard/auth-guard.service';
 import type { RequestWithUser } from '@/types/RequestWithUser';
+import { UpdateRideRequestData } from './dto/update-ride-request-data';
 
 @Controller('ride-request')
 export class RideRequestController {
@@ -19,11 +23,23 @@ export class RideRequestController {
   @UseGuards(AuthGuardService)
   @HttpCode(HttpStatus.CREATED)
   @Post()
-  async createUrl(
+  async createRideRequest(
     @Body() body: CreateRideRequestData,
     @Req() request: RequestWithUser,
   ) {
     const userId = request.user?.id;
     return await this.rideRequestService.create(userId!, body);
+  }
+
+  @UseGuards(AuthGuardService)
+  @HttpCode(HttpStatus.CREATED)
+  @Patch(':id')
+  async updateRideRequest(
+    @Body() body: UpdateRideRequestData,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Req() request: RequestWithUser,
+  ) {
+    const userId = request.user?.id;
+    return await this.rideRequestService.update(userId!, id, body);
   }
 }
--- src/ride-request/ride-request.service.ts
+++ src/ride-request/ride-request.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RideRequest } from './ride-request.entity';
 import { Repository } from 'typeorm';
 import { CreateRideRequestData } from './dto/create-ride-request-data';
+import { UpdateRideRequestData } from './dto/update-ride-request-data';
 
 @Injectable()
 export class RideRequestService {
@@ -26,5 +31,31 @@ export class RideRequestService {
     });
 
     return await this.rideRequestRepository.save(rideRequest);
+  }
+
+  async update(
+    userId: string,
+    request_id: string,
+    updateRideRequestData: UpdateRideRequestData,
+  ): Promise<{ message: string }> {
+    if (!request_id) {
+      throw new BadRequestException('Request id is required');
+    }
+
+    const existingRideRequest = await this.rideRequestRepository.findOne({
+      where: {
+        id: request_id,
+        passengerId: userId,
+      },
+    });
+
+    if (!existingRideRequest) {
+      throw new NotFoundException(
+        `Ride request with ID ${request_id} not found`,
+      );
+    }
+
+    await this.rideRequestRepository.update(request_id, updateRideRequestData);
+    return { message: 'Ride request has been updated successfully' };
   }
 }
