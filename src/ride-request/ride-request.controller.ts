35d50e38ba5a01b549a7dac907eec1e82f13202c
Laxman Rumba <rumbavai767@gmail.com>
2025-12-22 10:26:13 +0545

    refact: add ride delete and get service and controllers

---
+++ src/ride-request/ride-request.controller.ts
@@ -0,0 +1,94 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { RideRequestService } from './ride-request.service';
+import { CreateRideRequestData } from './dto/create-ride-request-data';
+import { AuthGuardService } from '@/auth-guard/auth-guard.service';
+import type { RequestWithUser } from '@/types/RequestWithUser';
+import { UpdateRideRequestData } from './dto/update-ride-request-data';
+
+@Controller('ride-request')
+export class RideRequestController {
+  constructor(private readonly rideRequestService: RideRequestService) {}
+
+  @UseGuards(AuthGuardService)
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  async createRideRequest(
+    @Body() body: CreateRideRequestData,
+    @Req() request: RequestWithUser,
+  ) {
+    const userId = request.user?.id;
+
+    if (!userId) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+
+    return await this.rideRequestService.create(userId, body);
+  }
+
+  @UseGuards(AuthGuardService)
+  @HttpCode(HttpStatus.CREATED)
+  @Patch(':id')
+  async updateRideRequest(
+    @Body() body: UpdateRideRequestData,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Req() request: RequestWithUser,
+  ) {
+    const userId = request.user?.id;
+
+    if (!userId) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+
+    return await this.rideRequestService.update(userId, id, body);
+  }
+
+  @UseGuards(AuthGuardService)
+  @HttpCode(HttpStatus.CREATED)
+  @Delete(':id')
+  async deleteRideRequest(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Req() request: RequestWithUser,
+  ) {
+    const userId = request.user?.id;
+
+    if (!userId) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+
+    return await this.rideRequestService.delete(userId, id);
+  }
+
+  @UseGuards(AuthGuardService)
+  @HttpCode(HttpStatus.CREATED)
+  @Get('my-rides')
+  async getAllRidesByUserId(@Req() request: RequestWithUser) {
+    const userId = request.decodedData.sub;
+
+    if (userId) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+
+    return await this.rideRequestService.getAllByUserId(userId);
+  }
+
+  @UseGuards(AuthGuardService)
+  @HttpCode(HttpStatus.CREATED)
+  @Get()
+  async getAllRides() {
+    return await this.rideRequestService.getAll();
+  }
+}
--- src/ride-request/ride-request.service.ts
+++ src/ride-request/ride-request.service.ts
@@ -56,28 +56,42 @@ export class RideRequestService {
       );
     }
 
+    if (existingRideRequest.isAccepted) {
+      throw new ConflictException('Accepted rides cannot be edited');
+    }
+
     const { departureStart, departureEnd, ...otherPayload } =
       updateRideRequestData;
 
+    if (
+      (departureStart && !departureEnd) ||
+      (!departureStart && departureEnd)
+    ) {
+      throw new BadRequestException(
+        'Both departureStart and departureEnd must be provided together',
+      );
+    }
+
     const updatedPayload = { ...otherPayload };
-    let currentRange = existingRideRequest.departureTime;
+    let departureTimeRange = existingRideRequest.departureTime;
     if (departureStart && departureEnd) {
       if (departureStart > departureEnd) {
         throw new BadRequestException('Start time must be before end time');
       }
-      currentRange = `[${departureStart.toISOString()}, ${departureEnd.toISOString()}]`;
+      departureTimeRange = `[${departureStart.toISOString()}, ${departureEnd.toISOString()}]`;
     }
 
-    const result = await this.rideRequestRepository
-      .createQueryBuilder()
-      .update(RideRequest)
-      .set({
+    const result = await this.rideRequestRepository.update(
+      {
+        id: request_id,
+        passengerId: userId,
+        isAccepted: false,
+      },
+      {
         ...updatedPayload,
-        departureTime: currentRange,
-      })
-      .where('id = :id', { id: request_id })
-      .andWhere('isAccepted = false')
-      .execute();
+        departureTime: departureTimeRange,
+      },
+    );
 
     if (result.affected === 0) {
       throw new ConflictException(
@@ -85,5 +99,88 @@ export class RideRequestService {
       );
     }
     return { message: 'Ride request has been updated successfully' };
+  }
+
+  async delete(
+    userId: string,
+    request_id: string,
+  ): Promise<{ message: string }> {
+    if (!request_id) {
+      throw new BadRequestException('Request id is required');
+    }
+
+    const existingRideRequest = await this.rideRequestRepository.findOne({
+      where: {
+        id: request_id,
+        passengerId: userId,
+      },
+    });
+
+    if (!existingRideRequest) {
+      throw new NotFoundException(
+        `Ride request with ID ${request_id} not found`,
+      );
+    }
+
+    await this.rideRequestRepository.softDelete({
+      id: request_id,
+      passengerId: userId,
+    });
+
+    return { message: 'Ride request has been cancelled successfully' };
+  }
+
+  async getById(
+    userId: string,
+    request_id: string,
+  ): Promise<{ message: string; ride: RideRequest }> {
+    if (!request_id) {
+      throw new BadRequestException('Request id is required');
+    }
+
+    const ride = await this.rideRequestRepository.findOne({
+      where: {
+        id: request_id,
+        passengerId: userId,
+      },
+      withDeleted: true,
+    });
+
+    if (!ride) {
+      throw new NotFoundException(
+        `Ride request with ID ${request_id} not found`,
+      );
+    }
+
+    return {
+      message: 'Ride request has been fetched successfully',
+      ride,
+    };
+  }
+
+  //to get all the ride or individual user
+  async getAllByUserId(
+    userId: string,
+  ): Promise<{ message: string; ride: RideRequest[] }> {
+    const ride = await this.rideRequestRepository.find({
+      where: { passengerId: userId },
+      withDeleted: true,
+    });
+
+    return {
+      message: 'Ride requests has been fetched successfully',
+      ride,
+    };
+  }
+
+  //to fetch all the pending ride request for riders/drivers
+  async getAll(): Promise<{ message: string; ride: RideRequest[] }> {
+    const ride = await this.rideRequestRepository.find({
+      where: { isAccepted: false },
+    });
+    return {
+      message: 'Ride requests has been fetched successfully',
+      ride,
+    };
   }
 }
